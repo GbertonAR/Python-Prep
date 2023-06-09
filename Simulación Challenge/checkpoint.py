7ba708b759a8b8eba3807cadbd2717c334acc45f
GBertonAR <gberton1967@gmail.com>
2023-06-09 15:59:32 -0300

    Esto es entrenamiento

--- Simulación Challenge/checkpoint.py
+++ Simulación Challenge/checkpoint.py
@@ -12,7 +12,19 @@ def Factorial(numero):
         Factorial(-2) debe retornar nulo
     '''
     #Tu código aca:
-    return 'Funcion incompleta'
+    if (type(numero) != int):
+        return None
+    if numero < 1:
+        return None
+    
+    aux_factorial=1
+    while numero > 1:
+        aux_factorial *= numero
+        numero -= 1
+        
+        
+    return aux_factorial    
+    
 
 def EsPrimo(valor):
     '''
@@ -25,8 +37,18 @@ def EsPrimo(valor):
         EsPrimo(7) debe retornar True
         EsPrimo(8) debe retornar False
     '''
-    #Tu código aca:
-    return 'Funcion incompleta'
+
+    if (type(valor) != int):
+        return None  
+    
+    primo = True
+    
+    for i in range (2,valor):
+        if ((valor%i) == 0):
+            primo = False
+            break
+    return primo    
+
     
 def ClaseAnimal(especie, color):
     '''
@@ -47,4 +69,16 @@ def ClaseAnimal(especie, color):
         a.CumpliAnios() -> debe devolver 3
     '''
     #Tu código aca:
+    class Animal:
+        def __init__(self, especie, color):
+            self.edad=0
+            self.especie=especie
+            self.color=color
+            
+        def CumplirAnios(self):
+            self.edad += 1
+            return self.edad
+            
+        
+    return Animal(especie,color)
     return 'Funcion incompleta'
